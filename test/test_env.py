c90ba61606bb882a3e039b89a11ea24d558df848
TKG1005 <seahopper12969@gmail.com>
2025-06-04 16:17:49 +0900

    fix pokemon_env

--- test/test_env.py
+++ test/test_env.py
@@ -1,4 +1,10 @@
 import numpy as np
+import sys
+from pathlib import Path
+ROOT_DIR = Path(__file__).resolve().parents[1]
+if str(ROOT_DIR) not in sys.path:
+    sys.path.insert(0, str(ROOT_DIR))
+
 from src.env.pokemon_env import PokemonEnv
 
 
