dedc203f7410c9547969329803cba8bacdd62c15
TKG1005 <seahopper12969@gmail.com>
2025-06-06 21:07:31 +0900

    a

--- src/env/pokemon_env.py
+++ src/env/pokemon_env.py
@@ -226,11 +226,13 @@ class PokemonEnv(gym.Env):
 
         client = getattr(self._env_player, "ps_client", None)
         if client is None:
+            print("クライアントがありません")
             return
 
         original = getattr(client, "_handle_message", None)
 
         if original is None:
+            print("該当メソッドがありません")
             return
 
         def _hook(message: str, *args: Any, **kwargs: Any) -> Any:
