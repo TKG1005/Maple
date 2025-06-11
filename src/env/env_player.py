f151d003139c45d29d669963c743d4bb79bca022
TKG1005 <seahopper12969@gmail.com>
2025-06-11 12:36:53 +0900

    Update env_player.py

--- src/env/env_player.py
+++ src/env/env_player.py
@@ -24,3 +24,29 @@ class EnvPlayer(Player):
 
         # 取得したインデックスを BattleOrder に変換して返す
         return self._env.action_helper.action_index_to_order(self, battle, action_idx)
+
+    #Playerクラスの_handle_battle_requestをオーバーライド(現状はそのままコピーしただけ)
+    async def _handle_battle_request(
+        self,
+        battle: AbstractBattle,
+        from_teampreview_request: bool = False,
+        maybe_default_order: bool = False,
+    ):
+        print(f"[DBG]_handle_battle_request() called batte.teampreview={battle.teampreview},from_teampreview_request = {from_teampreview_request}")
+        if maybe_default_order and (
+            "illusion" in [p.ability for p in battle.team.values()]
+            or random.random() < self.DEFAULT_CHOICE_CHANCE
+        ):
+            message = self.choose_default_move().message
+        elif battle.teampreview:
+            if not from_teampreview_request:
+                return
+            message = self.teampreview(battle)
+        else:
+            if maybe_default_order:
+                self._trying_again.set()
+            choice = self.choose_move(battle)
+            if isinstance(choice, Awaitable):
+                choice = await choice
+            message = choice.message
+        await self.ps_client.send_message(message, battle.battle_tag)
