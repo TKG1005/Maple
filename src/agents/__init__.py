06f664cbea9ab58232015c0aa3898a8232a3fe41
TKG1005 <122599381+TKG1005@users.noreply.github.com>
2025-06-16 10:57:26 +0900

    Add simple PolicyNetwork with PyTorch

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,7 +1,9 @@
 from .MapleAgent import MapleAgent
 from .maple_agent_player import MapleAgentPlayer
+from .policy_network import PolicyNetwork
 
 __all__ = [
     "MapleAgent",
     "MapleAgentPlayer",
+    "PolicyNetwork",
 ]
