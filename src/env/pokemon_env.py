bd30c21c5be1e0c4b33d8ddb98da39dff447629b
TKG1005 <122599381+TKG1005@users.noreply.github.com>
2025-06-19 22:42:33 +0900

    a

--- src/env/pokemon_env.py
+++ src/env/pokemon_env.py
@@ -35,7 +35,7 @@ class PokemonEnv(gym.Env):
         super().__init__()
 
         self.ACTION_SIZE = 10  # "gen9bss"ルールでは行動空間は10で固定
-        self.MAX_TURNS = 200  # エピソードの最大ターン数
+        self.MAX_TURNS = 1000  # エピソードの最大ターン数
 
         # Step10: 非同期アクションキューを導入
         # 数値アクションだけでなく、チーム選択コマンドなどの文字列も
