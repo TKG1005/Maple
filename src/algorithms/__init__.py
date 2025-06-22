4c2963dd37c7eea6a4e2b701804db131e3dee76c
TKG1005 <122599381+TKG1005@users.noreply.github.com>
2025-06-22 16:49:33 +0900

    Add compute_gae function with tests

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,9 +1,11 @@
 from .base import BaseAlgorithm
 from .reinforce import ReinforceAlgorithm
 from .dummy import DummyAlgorithm
+from .gae import compute_gae
 
 __all__ = [
     "BaseAlgorithm",
     "ReinforceAlgorithm",
     "DummyAlgorithm",
+    "compute_gae",
 ]
