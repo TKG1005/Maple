3fff819697a57feed519248d53172f61eb8ea21d
TKG1005 <122599381+TKG1005@users.noreply.github.com>
2025-06-22 20:24:45 +0900

    Revert "Implement PPO clip loss"

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -2,13 +2,10 @@ from .base import BaseAlgorithm
 from .reinforce import ReinforceAlgorithm
 from .dummy import DummyAlgorithm
 from .gae import compute_gae
-from .ppo import PPOAlgorithm, compute_ppo_loss
 
 __all__ = [
     "BaseAlgorithm",
     "ReinforceAlgorithm",
     "DummyAlgorithm",
-    "PPOAlgorithm",
     "compute_gae",
-    "compute_ppo_loss",
 ]
